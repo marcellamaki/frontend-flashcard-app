3a64f047d7c0a62089db1ac97f36b1c6e82bc0af
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-08 11:42:19 -0400

    added card flipping

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -9,13 +9,26 @@ class CardContainer extends React.Component {
 
     constructor(props){
       super(props);
+
+      this.state = {
+        flipped: false
+      }
+    }
+
+    handleFlip = () => {
+      this.setState({
+        flipped: !this.state.flipped
+      })
     }
 
     render() {
       return(
-          <div className="card">
+          <div className="card" onClick={this.handleFlip}>
+            {!this.state.flipped ?
             <CardFront question={this.props.question}/>
+              :
             <CardBack answer={this.props.answer}/>
+            }
           </div>
 
       )
