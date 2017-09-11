b6f5d529b76f1e3766a7e9d5233f16863d6cac63
github username <github email address>
2017-09-11 11:45:04 -0400

    added landing page image and centered title

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,13 +4,22 @@ import {Route, Link} from 'react-router-dom'
 class Welcome extends React.Component {
 
 
-
 	render() {
 
 		return(
+			<div className="pushable">
+			<div className="pusher">
+		    <div className="ui inverted vertical masthead center aligned segment">
+		        <div className="ui text container">
+		            <h1 className="ui inverted header center aligned">Card Flashr</h1>
+								<Link to="/flashcards"> Create a card deck </Link>
+		        </div>
+		    </div>
+		</div>
 			<div>
-				<Link to="/flashcards"> Create a card deck </Link>
-			</div>	
+				<img className="ui fluid image" src="https://images.unsplash.com/photo-1488722796624-0aa6f1bb6399?dpr=1&amp"/>
+			</div>
+		</div>
 		)
 	}
 
