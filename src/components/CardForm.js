c34a6569dbb6a3daceff4843fd329a756d73751a
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-12 10:34:39 -0400

    can create card and started deck form

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -2,26 +2,54 @@ import React from 'react';
 
 class CardForm extends React.Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      question: '',
+      answer: true,
+    }
+
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      question: event.target.value
+    })
+  }
+
   checkValue = (event) => {
-    console.log(event.target.value)
+    this.setState({
+      answer: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state)
   }
 
   render() {
     return (
-      <div className="ui form">
-          <div className="field">
-            <label>Question:</label>
-            <textarea></textarea>
-          </div>
-            <label>Answer</label>
-            <div className="ui radio checkbox">
-                <input type="radio" name="answer" tabIndex="0" value="true" onClick={this.checkValue}/>
-                <label>True</label>
+      <div>
+        <form className="ui form" onSubmit={this.handleSubmit}>
+            <div className="field" onChange={this.handleChange}>
+              <label>Question:</label>
+              <textarea></textarea>
             </div>
+            <div className="field">
+              <label>Answer:</label>
               <div className="ui radio checkbox">
-                  <input type="radio" name="answer" tabIndex="0" value="false" onClick={this.checkValue}/>
-                  <label>False</label>
+                  <input type="radio" name="answer" tabIndex="0" value={true} onClick={this.checkValue}/>
+                  <label>True</label>
               </div>
+                <div className="ui radio checkbox">
+                    <input type="radio" name="answer" tabIndex="0" value={false} onClick={this.checkValue}/>
+                    <label>False</label>
+                </div>
+            </div>
+            <button className="ui button" type="submit">Submit</button>
+        </form>    
       </div>
     )
   }
---
+++ src/components/DeckForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class DeckForm extends React.Component {
+
+	render(){
+		return(
+
+		)
+	}
+}
+
+export default DeckForm
