de8f32e2fc6426e5ffebc61116aa951ef23d4575
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-12 16:03:38 -0400

    added comment

--- src/components/DeckDropdown.js
+++ src/components/DeckDropdown.js
@@ -3,6 +3,7 @@ import React from 'react'
 const DeckDropdown = (props) => {
 		
 		return <option value={props.deck.id}>{props.deck.name}</option>
+		//hopefully this is working now!
 }
 
 export default DeckDropdown
